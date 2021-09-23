2e4ab6cc87913594a7a11837fa149e2687dc542a
Brett Sheleski <sheleski@fvtc.edu>
2021-09-23 11:48:42 -0500

    Moved CalculatorOperation to its own file.

---
+++ DelegateCalculator/CalculatorOperation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DelegateCalculator
+{
+    class CalculatorOperation
+    {
+        public CalculatorOperation(string displayText, Func<double, double, double> operationDelegate)
+        {
+            DisplayText = displayText;
+            OperationDelegate = operationDelegate;
+        }
+
+        public string DisplayText { get;  }
+        public Func<double, double, double> OperationDelegate { get; }
+    }
+}
--- DelegateCalculator/Program.cs
+++ DelegateCalculator/Program.cs
@@ -89,16 +89,4 @@ namespace DelegateCalculator
             return a / b;
         }
     }
-
-    class CalculatorOperation
-    {
-        public CalculatorOperation(string displayText, Func<double, double, double> operationDelegate)
-        {
-            DisplayText = displayText;
-            OperationDelegate = operationDelegate;
-        }
-
-        public string DisplayText { get;  }
-        public Func<double, double, double> OperationDelegate { get; }
-    }
 }
